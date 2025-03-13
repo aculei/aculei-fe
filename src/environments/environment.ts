b8007669000a2ba21ddd4885502827d38fee089c
mussida <96059227+mussida@users.noreply.github.com>
2025-03-14 00:33:52 +0100

    feat: add correct bucket url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: "https://aculei-be-org.onrender.com/v1/",
-  imageBaseUrl: "https://pub-75e1cb1a55624517a04334184db724e2.r2.dev/",
+  imageBaseUrl: "https:/bucket.aculei.xyz/",
 };
