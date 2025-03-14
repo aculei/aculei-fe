7dce6c01a0c8ce0df6ce71398dbbf90cd23042a1
micheledinelli <dinellimichele00@gmail.com>
2025-03-14 18:02:04 +0100

    feat: map about page in navbar and routes initializing components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./pages/home/home.component";
 import { ArchiveComponent } from "./pages/archive/archive.component";
+import { AboutComponent } from "./pages/about/about.component";
 import { ArchiveDetailComponent } from "./pages/archive-detail/archive-detail.component";
 import { ExperienceComponent } from "./pages/experience/experience.component";
 
@@ -12,4 +13,5 @@ export const routes: Routes = [
     component: ArchiveDetailComponent,
   },
   { path: "experience", component: ExperienceComponent },
+  { path: "about", component: AboutComponent },
 ];
