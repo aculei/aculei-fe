f3944695be44fca788faa08016bd703e3e8b1182
Michele <95191347+micheledinelli@users.noreply.github.com>
2025-03-14 12:29:10 +0100

    feat: add mobile component which is rendered at root if user agent is mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,40 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { NgIf } from "@angular/common";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { MobileComponent } from "./components/mobile/mobile.component";
 
 @Component({
-    selector: "app-root",
-    imports: [RouterOutlet, NavbarComponent, FooterComponent],
-    templateUrl: "./app.component.html",
-    styleUrl: "./app.component.css"
+  selector: "app-root",
+  imports: [
+    RouterOutlet,
+    NgIf,
+    NavbarComponent,
+    FooterComponent,
+    MobileComponent,
+  ],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.css",
 })
-export class AppComponent {
-  title = "";
+export class AppComponent implements OnInit, OnDestroy {
+  isMobile = false;
+
+  ngOnInit(): void {
+    this.checkIfMobile();
+    window.addEventListener("resize", this.checkIfMobile);
+  }
+
+  ngOnDestroy(): void {
+    window.removeEventListener("resize", this.checkIfMobile);
+  }
+
+  checkIfMobile = (): void => {
+    const isMobileAgent = /Mobi|iPhone|iPad|iPod|Android/i.test(
+      navigator.userAgent
+    );
+    const isMobileWidth = window.innerWidth < 768;
+
+    this.isMobile = isMobileAgent || isMobileWidth;
+  };
 }
