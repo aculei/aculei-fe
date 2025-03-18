758ed80f036e7215062e6aca325af75cfe512c70
micheledinelli <dinellimichele00@gmail.com>
2025-03-18 15:20:06 +0100

    feat: update mobile landing

--- src/app/components/mobile/mobile.component.ts
+++ src/app/components/mobile/mobile.component.ts
@@ -1,8 +1,17 @@
+import { NgStyle } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-mobile",
   templateUrl: "./mobile.component.html",
   styleUrls: ["./mobile.component.css"],
+  imports: [NgStyle],
 })
-export class MobileComponent {}
+export class MobileComponent {
+  imageUrl: string;
+  constructor() {
+    this.imageUrl = `/assets/mobile-background-${
+      Math.floor(Math.random() * 3) + 1
+    }.webp`;
+  }
+}
