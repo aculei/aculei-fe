02126ade766d04e9f9eaa29132244d530e237926
micheledinelli <dinellimichele00@gmail.com>
2025-03-19 22:16:44 +0100

    feat(navbar): update copy dynamically reading the current url

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,30 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import { RouterLink, RouterLinkActive, RouterModule } from "@angular/router";
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterModule,
+  NavigationEnd,
+} from "@angular/router";
 
 @Component({
-    selector: "app-navbar",
-    imports: [RouterModule, RouterLink, RouterLinkActive, CommonModule],
-    templateUrl: "./navbar.component.html",
-    styleUrl: "./navbar.component.css"
+  selector: "app-navbar",
+  imports: [RouterModule, RouterLink, RouterLinkActive, CommonModule],
+  templateUrl: "./navbar.component.html",
+  styleUrl: "./navbar.component.css",
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  currentRoute: string = "";
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.currentRoute = event.urlAfterRedirects;
+        console.log(this.currentRoute);
+      }
+    });
+  }
+}
