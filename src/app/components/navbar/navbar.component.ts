605173a9a84b3a0042e7fe96913d6f66d5c1a888
micheledinelli <dinellimichele00@gmail.com>
2025-03-20 16:07:31 +0100

    refactor(navbar): remove conditionally rendered copy

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,6 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
-import {
-  Router,
-  RouterLink,
-  RouterLinkActive,
-  RouterModule,
-  NavigationEnd,
-} from "@angular/router";
+import { RouterLink, RouterLinkActive, RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-navbar",
@@ -14,17 +8,4 @@ import {
   templateUrl: "./navbar.component.html",
   styleUrl: "./navbar.component.css",
 })
-export class NavbarComponent {
-  currentRoute: string = "";
-
-  constructor(private router: Router) {}
-
-  ngOnInit(): void {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute = event.urlAfterRedirects;
-        console.log(this.currentRoute);
-      }
-    });
-  }
-}
+export class NavbarComponent {}
