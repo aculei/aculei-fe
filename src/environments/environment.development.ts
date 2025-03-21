b46f77cfd07958d169bbe8716a238db9ad25740e
micheledinelli <dinellimichele00@gmail.com>
2025-03-21 11:20:06 +0100

    Revert "chore: migrate backend url to zrok"
    This reverts commit de0c52f623d11e395538c66853dcc222c4b087df.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
-  // apiUrl: "https://aculei-be-org.onrender.com/v1/",
-  apiUrl: "https://aculei.share.zrok.io/v1/",
+  apiUrl: "https://aculei-be-org.onrender.com/v1/",
   imageBaseUrl: "https://bucket.aculei.xyz/",
 };
