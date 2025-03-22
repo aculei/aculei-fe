546fb4ec26999c63c0fd86bbfae2e12d07bbbbb9
micheledinelli <dinellimichele00@gmail.com>
2025-03-22 18:26:34 +0100

    chore: update backend url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  apiUrl: "https://aculei-be-org.onrender.com/v1/",
+  // apiUrl: "https://aculei-be-org.onrender.com/v1/",
+  apiUrl: "http://backend.aculei.xyz/v1/",
   imageBaseUrl: "https://bucket.aculei.xyz/",
 };
